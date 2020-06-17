82650c99b60ec358425765a5c04ac2662140a02f
Marion Candelier <marion.candelier@telecom-paristech.fr>
2020-06-17 16:59:21 +0200

    select mode

--- site/js/visualizations.js
+++ site/js/visualizations.js
@@ -2,30 +2,33 @@ var svg;
 
 const canvas_h = getComputedStyle(container).height;
 const canvas_w = getComputedStyle(container).width;
-const p = document.createElement("p");
-p.className = "text_visu";
 const divgrosse = document.createElement("div");
 const div1 =  document.createElement("div");
 const div2 =  document.createElement("div");
 const div3  = document.createElement("div");
 const div4  = document.createElement("div");
+const boutton = document.createElement("button");
+boutton.id = "see";
+boutton.onclick = showVisu;
 
 function initVisualization(){
     clearInterval(wave_id);
     container.innerHTML = "";
     container.appendChild(divgrosse);
+    boutton.innerText = "See"
+    container.appendChild(boutton);
     div1.id = "left";
     div2.id = "center";
     div3.id = "right";
     div4.id ="right_right";
-    div1.innerHTML="Select the abscissa : <div><input type='radio' id='price' name='drone' value='price'checked><label for='price'> Price</label></div> <div><input type='radio' id='volume' name='drone' value='volume'> <label for='volume'>Volume</label></div><div><input type='radio' id='weight' name='drone' value='weight'><label for='weight'> Weight</label></div><div><input type='radio' id='age' name='drone' value='age'checked><label for='age'> Age</label></div>";
-    div2.innerHTML="Select the ordinate : <div><input type='radio' id='price' name='drone' value='price'checked><label for='price'> Price</label></div> <div><input type='radio' id='volume' name='drone' value='volume'> <label for='volume'>Volume</label></div><div><input type='radio' id='weight' name='drone' value='weight'><label for='weight'> Weight</label></div><div><input type='radio' id='age' name='drone' value='age'checked><label for='age'> Age</label></div>";
-    div3.innerHTML = "Select your mode : <div><input type='radio' id='gbm' name='drone' value='gbm'checked><label for='gbm'> Girl Boy Mixt </label></div> <div><input type='radio' id='colors' name='drone' value='colors'> <label for='colors'>Main color</label></div>";
+    div1.innerHTML="Select the abscissa : <div><input type='radio' id='price' name='abs' value='price'checked><label for='price'> Price</label></div> <div><input type='radio' id='volume' name='abs' value='volume'> <label for='volume'>Volume</label></div><div><input type='radio' id='weight' name='abs' value='weight'><label for='weight'> Weight</label></div><div><input type='radio' id='age' name='abs' value='age'checked><label for='age'> Age</label></div>";
+    div2.innerHTML="Select the ordinate : <div><input type='radio' id='price' name='od' value='price'checked><label for='price'> Price</label></div> <div><input type='radio' id='volume' name='od' value='volume'> <label for='volume'>Volume</label></div><div><input type='radio' id='weight' name='od' value='weight'><label for='weight'> Weight</label></div><div><input type='radio' id='age' name='od' value='age'checked><label for='age'> Age</label></div>";
+    div3.innerHTML = "Select your mode : <div><input type='radio' id='gbm' name='mode' value='gbm'checked><label for='gbm'> Girl Boy Mixt </label></div> <div><input type='radio' id='colors' name='mode' value='colors'> <label for='colors'>Main color</label></div>";
+ 
     divgrosse.appendChild(div1);
     divgrosse.appendChild(div2);
     divgrosse.appendChild(div3);
     divgrosse.appendChild(div4);
-    container.appendChild(p);
     svg = d3.select(container)
             .append('svg')
             .attr("width", canvas_w)
@@ -37,6 +40,68 @@ function initVisualization(){
 function color(c){
     return(d3.hsv(c*360,1,1))
 }
+
+function showVisu(){
+    d3.select("svg").remove();
+    svg = d3.select(container)
+            .append('svg')
+            .attr("width", canvas_w)
+            .attr("height", canvas_h)
+            .attr("class", "canvas")
+    console.log("visu visu");
+    var radios = document.getElementsByName('mode');
+    console.log(radios)
+    var valeurmode;
+
+    for(var i = 0; i < radios.length; i++){
+        console.log(radios[1].checked);
+        if(radios[i].checked){
+            valeurmode = radios[i].value;
+        }
+    }
+
+    console.log(valeurmode);
+
+    let x = d3.scaleLinear()
+            .domain(d3.extent(database, (d) => d.poids))
+            .range([20, parseFloat(canvas_w)-20]);
+    
+    let y = d3.scaleLinear()
+            .domain(d3.extent(database, (d) => d.prix))
+            .range([20, parseFloat(canvas_h)-20]);
+    
+
+    if (valeurmode=="gbm"){
+        let colorgbm = d3.scaleOrdinal()
+            .domain(['Boy', 'Girl', 'Mixte'])
+            .range(['blue', 'pink', 'grey'])
+
+        svg.selectAll('circle')
+        .data(database)
+        .enter()
+        .append('circle')
+        .attr('r', 3)
+        .attr('cx', (d) => x(d.poids))
+        .attr('cy', (d) => y(d.prix))
+        .attr('fill', (d) => colorgbm( d.genre))
+        .on("mouseover",function(d){div4.innerHTML = "Description du jouet : <br> </br>" + "Nom : " +  (d.nom).toLowerCase() +"<br> </br> Prix : " + (d.prix) });
+        wave_id = setInterval(() => waveContent(secondary_nav, "tab"), 3000);
+    }
+
+    if(valeurmode=="colors"){
+        svg.selectAll('circle')
+        .data(database)
+        .enter()
+        .append('circle')
+        .attr('r', 3)
+        .attr('cx', (d) => x(d.poids))
+        .attr('cy', (d) => y(d.prix))
+        .attr('fill', (d) => color( +d.couleur))
+        .on("mouseover",function(d){div4.innerHTML = "Description du jouet : <br> </br>" + "Nom : " +  (d.nom).toLowerCase() +"<br> </br> Prix : " + (d.prix) });
+        wave_id = setInterval(() => waveContent(secondary_nav, "tab"), 3000);
+    }
+}
+
 
 function showVisualisation1(){
     initVisualization();
